72eb449678b4bf341c14b00d7547e9089a07ab3e
Angelo <angelo.rendina@prima.it>
2022-12-01 14:06:32 +0000

    day 1 annotated and no heap allocations

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,75 +1,70 @@
-use std::cmp::Ordering;
-use std::collections::BTreeSet;
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-
-pub fn star_one(filename: &str) -> u64 {
-    read_calories(filename)
-        .into_iter()
-        .map(|elf| elf.into_iter().sum())
-        .max()
-        .expect("Vector is empty")
+/// Structure responsible for storing the total calories of N elves.
+/// These are kept in descending order.
+///
+/// Everything is stack-allocated.
+struct ChunkiestElves<const N: usize> {
+    elves: [u64; N],
 }
 
-pub fn star_two(filename: &str) -> u64 {
-    #[derive(PartialEq, Eq)]
-    struct Elf {
-        calories: u64,
-        index: usize,
+impl<const N: usize> ChunkiestElves<N> {
+    /// Reads an empty-string separated list of calories,
+    /// aggregating by elf and remembering the top N.
+    ///
+    /// See `mock.txt` or `input.txt` for examples on the formatting.
+    fn parse(values: &str) -> Self {
+        let mut this = Self { elves: [0; N] };
+
+        // current aggregate
+        let mut elf = 0;
+
+        for line in values.lines() {
+            if line.is_empty() {
+                // on empty lines, store the aggregated value, and start a new one
+                this.push(elf);
+                elf = 0;
+            } else {
+                // increase the aggregate by the value in this line
+                let calories = line.parse::<u64>().expect("Cannot parse value");
+                elf += calories;
+            }
+        }
+
+        // remember to store the last elf!
+        // empty lines at the end of the file are ignored when iterating with `.lines()`
+        this.push(elf);
+
+        this
     }
 
-    impl PartialOrd for Elf {
-        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-            Some(self.cmp(other))
+    /// Consider the given Elf, and store the value at the correct position if it is amongst the top N.
+    fn push(&mut self, mut elf: u64) {
+        // the algorithm keeps higher values in place, and replaces the first lower value with the target;
+        // this lower value is the new target, and we keep iterating down the ordered list
+        // repeating the process as needed.
+        // Essentially, this is inserting the value at the right place and shifting lower values further
+        for i in 0..N {
+            if self.elves[i] < elf {
+                // SAFETY: elves has exactly N elements
+                std::mem::swap(unsafe { self.elves.get_unchecked_mut(i) }, &mut elf);
+            }
         }
     }
-
-    impl Ord for Elf {
-        fn cmp(&self, other: &Self) -> Ordering {
-            self.calories.cmp(&other.calories)
-        }
-    }
-
-    read_calories(filename)
-        .into_iter()
-        .enumerate()
-        .map(|(index, calories)| Elf {
-            calories: calories.into_iter().sum::<u64>(),
-            index,
-        })
-        .collect::<BTreeSet<Elf>>()
-        .into_iter()
-        .rev()
-        .take(3)
-        .map(|elf| elf.calories)
-        .sum::<u64>()
 }
 
-fn read_calories(filename: &str) -> Vec<Vec<u64>> {
-    let mut calories_by_elf: Vec<Vec<u64>> = vec![];
-    let mut current_elf: Option<&mut Vec<u64>> = None;
+pub fn star_one() -> u64 {
+    #[cfg(test)]
+    let values = include_str!("mock.txt");
+    #[cfg(not(test))]
+    let values = include_str!("input.txt");
+    ChunkiestElves::<1>::parse(values).elves[0]
+}
 
-    let file = File::open(filename).expect("Cannot open file");
-    for line in io::BufReader::new(file).lines() {
-        let line = line.expect("Cannot read line");
-        if line.is_empty() {
-            current_elf = None;
-        } else {
-            let elf = match current_elf.take() {
-                Some(elf) => elf,
-                None => {
-                    calories_by_elf.push(vec![]);
-                    calories_by_elf.last_mut().expect("Vector is empty")
-                }
-            };
-            let food = line.parse().expect("Cannot parse value");
-            elf.push(food);
-            current_elf = Some(elf);
-        }
-    }
-
-    calories_by_elf
+pub fn star_two() -> u64 {
+    #[cfg(test)]
+    let values = include_str!("mock.txt");
+    #[cfg(not(test))]
+    let values = include_str!("input.txt");
+    ChunkiestElves::<3>::parse(values).elves.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -78,11 +73,46 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one("src/day1/mock.txt"), 24000);
+        assert_eq!(star_one(), 24000);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two("src/day1/mock.txt"), 45000);
+        assert_eq!(star_two(), 45000);
+    }
+
+    #[test]
+    fn fuzzy_push_chunkiest_elves() {
+        // do 1000 fuzzy tests
+        for _ in 0..1000 {
+            let mut chonks = ChunkiestElves { elves: [0; 3] };
+
+            // generate a sample of variable length (0 to 255)
+            let sample_size: u8 = rand::random();
+            let sample_size = sample_size as usize;
+            // sample is made of uniformly distributed u64 values
+            let mut sample = Vec::<u64>::with_capacity(sample_size);
+            for _ in 0..sample_size {
+                let value: u64 = rand::random();
+                sample.push(value);
+                // push the value to chonks!
+                // this should keep the highest three, descendingly ordered
+                chonks.push(value);
+            }
+
+            // now sort the whole sample descendingly
+            sample.sort();
+            sample.reverse();
+
+            // consider the highest three values from the sample, and compare with the elves in chonks
+            // since the samples could be fewer than 3, we take what's there and default to 0 if not enough
+            let expected = (0..3).map(|i| sample.get(i).unwrap_or(&0));
+            let actual = chonks.elves.iter();
+
+            // they should match!
+            for (expected, actual) in expected.zip(actual) {
+                assert_eq!(expected, actual);
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("1.1: {}", day1::star_one("src/day1/input.txt"));
-    println!("1.2: {}", day1::star_two("src/day1/input.txt"));
+    println!("1.1: {}", day1::star_one());
+    println!("1.2: {}", day1::star_two());
 }
